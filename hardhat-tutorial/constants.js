20426501a1f4aa90cb3c3778e6cf8970e81bf544
huangzhiyong <hot3246624@163.com>
2023-01-12 19:37:58 +0800

    create DAO

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xd634661d8740D6D4A9815342fd35689BEb940C26";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
